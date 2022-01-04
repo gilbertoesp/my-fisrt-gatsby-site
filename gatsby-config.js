cf31a4f4690d22a3fd6f7c12bdf06fb43f95014a
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2022-01-03 22:01:36 -0700

    Agregando una imagen, es mi fondo de pantalla

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "My Fisrt Gatsby Page",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const Homepage = () => {
   return (
     <main>
       <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a redish=brown pitbull, posing on a couch"
+        src="../images/wp.png"
+      />
       </Layout>
     </main>
   )
